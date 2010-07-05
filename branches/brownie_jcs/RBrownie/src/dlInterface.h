3226251a3b57c9be18b594392d39b67e59b97b65
conrad.stack <conrad.stack@d0513182-7e4c-0410-8651-4525cabb6473>
2010-07-05 06:36:25 +0000

    -Added interface functions to return TAXA, ASSUMPTIONS, TREES, and CHARACTER (both?) -NOTE: I'm assuming that Rcpp/R is okay with STL containers.  Some of the dynamic memory allocation might cause problems (I'm pretty sure the Rcpp introduction said this was supported)

--- branches/brownie_jcs/RBrownie/src/dlInterface.h
+++ branches/brownie_jcs/RBrownie/src/dlInterface.h
@@ -14,11 +14,23 @@ class dlInterface
 	public:
 	dlInterface();
 	~dlInterface();
-	//int getCharNumber();
 	void pipe( std::string );
 	void execute(std::string);
 	int getNumLoadedTrees();
-	std::string getTree(int);
+	int getNumTaxa();
+	int getNumDiscreteChars();
+	int getNumChars();
+	std::vector<std::string> getCharLabels();
+	std::vector<char> getDiscreteChar(int colindex=0);
+	std::vector<float> getContChar(int colindex=0);
+	//TODO: add indexing by character name?
+		
+	int getNumTaxaSets();
+	std::vector<std::string> getTaxaSetNames();
+	std::vector< std::vector<std::string> > getTaxaSets();
+	
+	std::string getTree(int,bool=true);  // these two might not return the same trees.
+	bool getTree(int, std::ostream &f, bool=true);
 	float getTreeWeight(int);
 	bool writeTrees(std::string);
 };
