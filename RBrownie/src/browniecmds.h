1bf21cb45f101cd5b009003b379e618996f7ed47
conrad.stack <conrad.stack@d0513182-7e4c-0410-8651-4525cabb6473>
2010-07-06 04:15:38 +0000

    -rcpp_hello_world.cpp contains function that reads back entire loaded object from BROWNIE

---
+++ RBrownie/src/browniecmds.h
@@ -0,0 +1,15 @@
+#ifndef _RBrownie_BROWNIECMDS_H
+#define _RBrownie_BROWNIECMDS_H
+
+#include <string>
+
+/* Brownie string commands.  All in one place so they need to be changed only once
+ * 
+ * note: All commands here are incomplete.  At least, some will need a semi-colon on the end
+ *
+ */
+const std::string EXECUTE = "execute ";
+const std::string SETLOG = "log file=";
+const std::string STOPLOG = "log stop";
+
+#endif
