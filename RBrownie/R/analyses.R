4a1fbd9c32720ab35090225b892ac09309776aaf
conrad.stack <conrad.stack@d0513182-7e4c-0410-8651-4525cabb6473>
2010-07-25 23:11:56 +0000

    -Changed the way commands(...) works, added options that allow you to replace commands (by index) -Added functions to check how many trees in a list are mono/paraphyletic for a certain taxaset -Added functions which add analyses to a list of trees (Noncensored and Censored).

--- RBrownie/R/analyses.R
+++ RBrownie/R/analyses.R
@@ -11,6 +11,8 @@
 # ?
 
 
+
+
 # Method to do ancestral state reconstruction in brownie
 #
 # return  
@@ -65,6 +67,183 @@ asr <- function(phytree,file="",usedata,extended=TRUE)
 	}
 	return( outtree )
 }
+
+###### Add to commands list:
+#
+addcmd.taxaset <- function(obj,cmdobj,x)
+{
+	# set 'taxset' option:
+	tsnames = taxaset.names(obj)
+	if(is.character(x))
+	{
+		# TODO: add new taxaset if it doesn't exist.
+		if(x != "ALL" && !(x %in% tsnames))
+		{
+			stop("Specified taxaset,",x,", is not a defined taxaset.")
+		} else {
+			cmdobj$options = rbind(cmdobj$options,c("taxset",x))
+		}
+	} else {
+		if(!is.integer(x) || x > length(tsnames))
+		{
+			stop("Valid taxaset was not specified: ",x)
+		} else {
+			cmdobj$options = rbind(cmdobj$options,c("taxset",tsnames[x]))
+		}
+	}
+	return(cmdobj)
+}
+
+addcmd.binary <- function(cmdobj,operand1,boolval)
+{
+	cmdobj$options = rbind(cmdobj$options,c(operand1,ifelse(boolval,"yes","no")  ))
+	return(cmdobj)
+}
+
+addcmd.literal <- function(cmdobj,operand1,litval)
+{
+	cmdobj$options = rbind(cmdobj$options,c(operand1,litval))
+	return(cmdobj)
+}
+
+
+# Non-censored rate test:
+# arguments are options that can be specified:
+#
+# ... arguments for the commands(x,...) call.  add=? index=? replace=?
+# 		allows the user to specify where this command should go.
+#
+addNonCensored <- function(obj,
+							file=NULL,
+							taxset="ALL",
+							treeloop=FALSE,
+							charloop=FALSE,
+							fileappend=FALSE,
+							filereplace=FALSE,
+							usetempfile=FALSE,
+							...)
+{
+	
+	# convert to list for compatibility:
+	if(!is.list(obj)){
+		obj = list(obj)
+	}
+	
+	if(hasCommands(obj[[1]]))
+	{
+		cmds = commands(obj[[1]])
+		cat("New command being added after current ones:\n")
+		for(jj in seq(length(cmds)))
+			cat(cmds[jj],"\n")
+		
+	}
+	
+	# setup new command object:
+	#
+	newcmd = list(command=NULL,options=matrix(NA,ncol=2,nrow=0))
+	newcmd$command = "cont"
+	
+	# add options
+	newcmd = addcmd.taxaset(obj[[1]],newcmd,taxset) # taxaset
+	newcmd = addcmd.binary(newcmd,"treeloop",treeloop) # treeloop
+	newcmd = addcmd.binary(newcmd,"charloop",charloop) # charloop
+	if(!usetempfile && !is.null(file) && is.character(file)) # file, append, replace
+	{
+		if(!file.exists(file))
+			stop("Specified file,",file,", does not exist.")
+		
+		newcmd = addcmd.literal(newcmd,"file",file[1])
+		newcmd = addcmd.binary(newcmd,"Append",fileappend)
+		newcmd = addcmd.binary(newcmd,"Replace",filereplace)
+		
+	} else {
+		if(usetempfile)
+		{
+			newcmd = addcmd.literal(newcmd,"file",tempfile())
+			newcmd = addcmd.binary(newcmd,"Append",FALSE)
+			newcmd = addcmd.binary(newcmd,"Replace",TRUE)		
+		} else {
+			newcmd = addcmd.literal(newcmd,"file","")
+		}
+	}
+
+	# add commands to all members of the list:
+	#
+	for(ii in seq(length(obj)))
+	{
+		commands(obj[[ii]],...) <- write.brownie.string(newcmd)
+	}
+	
+	if(length(obj) == 1)
+		obj = obj[[1]]
+	
+	return(obj)
+}
+
+
+
+##### Censored Rate test:
+#
+addCensored <- function(obj,
+						file=NULL,
+						reps=0,
+						taxset="ALL",
+						treeloop=FALSE,
+						charloop=FALSE,
+						quiet=TRUE,
+						usetempfile=FALSE,
+						...)
+{
+	###### initialize
+	if(!is.list(obj)){
+		obj = list(obj)
+	}							
+	
+	###### newcmd
+	newcmd = list(command=NULL,options=matrix(NA,ncol=2,nrow=0))
+	newcmd$command = "ratetest"
+	
+	newcmd = addcmd.taxaset(obj[[1]],newcmd,taxset) # taxaset
+	newcmd = addcmd.literal(newcmd,"reps",as.character(reps))
+	newcmd = addcmd.binary(newcmd,"treeloop",treeloop) # treeloop
+	newcmd = addcmd.binary(newcmd,"charloop",charloop) # charloop
+	newcmd = addcmd.binary(newcmd,"quiet",quiet) # charloop
+	if(!usetempfile && !is.null(file) && is.character(file)) # file, append, replace
+	{
+		if(!file.exists(file))
+			stop("Specified file,",file,", does not exist.")
+		
+		newcmd = addcmd.literal(newcmd,"file",file[1])		
+	} else {
+		if(usetempfile)
+		{
+			newcmd = addcmd.literal(newcmd,"file",tempfile())
+		} else {
+			newcmd = addcmd.literal(newcmd,"file","")
+		}
+	}
+	
+	
+	# TODO: -Check for all trees whether taxasets are monophyletic or paraphyletic.
+	#		 warn if some trees are not either.
+	if(!all(sapply(obj,areTaxaMono,taxset)))
+		warning("In some trees the taxaset,",taxset,", does not form a monophyletic clade.  Might want to remove those trees before running this analysis")
+	
+	
+	# add commands to all members of the list:
+	#
+	for(ii in seq(length(obj)))
+	{
+		commands(obj[[ii]],...) <- write.brownie.string(newcmd)
+	}
+	
+	###### return
+	if(length(obj) == 1)
+		obj = obj[[1]]
+	
+	return(obj)	
+}
+
 
 
 
