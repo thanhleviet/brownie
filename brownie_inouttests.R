efb1b3639e24024e8e0888593292e68524df7bef
conrad.stack <conrad.stack@d0513182-7e4c-0410-8651-4525cabb6473>
2010-08-02 04:13:02 +0000

    input/output tests

---
+++ brownie_inouttests.R
@@ -0,0 +1,20 @@
+setwd("C:/Users/conrad/brownie/tests")
+require(RBrownie)
+
+# Discrete tests ("discrete")
+junk = readBrownie("geospiza.nex")
+junkrun=runDiscrete(junk,brfile="disc_junk.txt",
+models=c("nonrev","nonrev"),freqs=c("unif","equilib"),reconstruct=T)
+junkrun
+plot(junkrun$trees[[1]])
+
+# Noncensored ("cont")
+junk = readBrownie("parrot.nex")
+junkrun=runNonCensored(junk,brfile="cont_junk.txt",models=brownie.models.continuous()[1:2],treeloop=T,charloop=T)
+junkrun
+
+# Censored ("ratetest")
+junk = readBrownie("parrot.nex")
+junkrun = runCensored(junk,brfile="ratetest_junk.txt",taxset="intrajoint",reps=1000,charloop=T)
+junkrun
+
